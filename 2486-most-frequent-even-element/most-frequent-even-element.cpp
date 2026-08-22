77d06d06ead75f34857d6f1c0120236eca608c24
Krishna <krishnamall1120@gmail.com>
2026-08-22 20:43:59 +0530

    Time: 27 ms (53.11%) | Memory: 41.7 MB (78.67%) - LeetSync

--- 2486-most-frequent-even-element/most-frequent-even-element.cpp
+++ 2486-most-frequent-even-element/most-frequent-even-element.cpp
@@ -3,12 +3,13 @@ public:
     int mostFrequentEven(vector<int>& nums) {
         int ans = -1;
         unordered_map<int,int>m;
-        m[ans]=0;
+        m.reserve(nums.size());
+        int max_freq = 0;
         for(int i=0; i<nums.size(); i++){
             if(nums[i]%2==0){
                 m[nums[i]]++;
-                if(m[ans]<m[nums[i]]) ans = nums[i];
-                else if(m[ans]==m[nums[i]] && ans > nums[i]) ans = nums[i];
+                if(max_freq<m[nums[i]]) ans = nums[i] , max_freq = m[nums[i]];
+                else if(max_freq==m[nums[i]] && ans > nums[i]) ans = nums[i] , max_freq = m[nums[i]];
             }
         }
         return ans;
