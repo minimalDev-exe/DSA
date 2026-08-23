ab1db39e62db4fefc4c106f30fc70e9436d573ef
Krishna <krishnamall1120@gmail.com>
2026-08-24 01:02:29 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (45.86%) - LeetSync

--- 4381-check-ascii-palindromic/check-ascii-palindromic.cpp
+++ 4381-check-ascii-palindromic/check-ascii-palindromic.cpp
@@ -4,23 +4,8 @@ public:
         string bin = "";
         for(int i=0; i<s.size(); i++){
             int ascii = int(s[i]);
-            string temp = "";
-            int pushed = 0;
-            while(ascii>0){
-                int rem = ascii%2;
-                temp.push_back(rem + '0');
-                pushed++;
-                ascii/=2;
-            }
-            if(pushed<8){
-                int n = 8-pushed;
-                while(n--){
-                    temp.push_back('0');
-                    
-                }
-            }
-            reverse(temp.begin(),temp.end());
-            bin = bin + temp;
+            string temp = bitset<8>(ascii).to_string();
+            bin+=temp;
         }
         int lt = 0 , rt = bin.size()-1;
         while(lt<=rt){
