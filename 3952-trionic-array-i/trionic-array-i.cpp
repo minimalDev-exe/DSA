14eba3585d9e93054ac5772ea29f55ceebfaa3eb
Krishna <krishnamall1120@gmail.com>
2026-08-28 16:29:47 +0530

    Time: 1 ms (10.83%) | Memory: 27.7 MB (41.20%) - LeetSync

--- 3952-trionic-array-i/trionic-array-i.cpp
+++ 3952-trionic-array-i/trionic-array-i.cpp
@@ -1,28 +1,30 @@
 class Solution {
 public:
-    bool isTrionic(vector<int>& nums) {
+    bool isDecreasing(vector<int>&nums , int peak , int valley){
         int n = nums.size();
-        if(n<4) return false;
-        if(nums[0]>nums[1]) return false;
-        bool seg_1 = false , seg_2 = false , seg_3 = false;
-        for(int i=0; i<n-1; i++){
-            if(seg_1==false && seg_2==false && seg_3==false && nums[i]<nums[i+1]){
-                seg_1 = true;
-            }
-            else if(seg_1==true && seg_2==false && seg_3==false && nums[i]>nums[i+1]){
-                seg_2 = true;
-            }
-            else if(seg_1==true && seg_2==true && seg_3==false && nums[i]<nums[i+1]){
-                seg_3 = true;
-            }
-            else if(nums[i]==nums[i+1]){
-                return false;
-            }
-            else if(seg_1==true && seg_2==true && seg_3==true && nums[i]>nums[i+1]){
+        if(peak==0 || valley==n-1) return false;
+        for(int i=peak; i<valley; i++){
+            if(nums[i]<=nums[i+1]){
                 return false;
             }
         }
-        if(seg_1 && seg_2 && seg_3) return true;
-        else return false;
+        return true;
+    }
+
+    bool isTrionic(vector<int>& nums) {
+        int n = nums.size();
+        int peak = n-1 , valley = 0;
+        for(int i=0; i<n-1; i++){
+            if(peak==n-1 && nums[i]>=nums[i+1]){
+                peak = i;
+            }
+            if(valley==0 && nums[n-1-i]<=nums[n-2-i]){
+                valley = n-1-i;
+            }
+            if(peak<valley){
+                return isDecreasing(nums , peak , valley);
+            }
+        }
+        return false;
     }
 };
