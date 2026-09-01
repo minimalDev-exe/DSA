9df7377e01bd898334d36f56258286c6950e76e8
Krishna <krishnamall1120@gmail.com>
2026-09-01 19:08:55 +0530

    Time: 0 ms (100.00%) | Memory: 78.4 MB (26.11%) - LeetSync

--- 2006-find-the-student-that-will-replace-the-chalk/find-the-student-that-will-replace-the-chalk.cpp
+++ 2006-find-the-student-that-will-replace-the-chalk/find-the-student-that-will-replace-the-chalk.cpp
@@ -1,31 +1,18 @@
 class Solution {
 public:
     int chalkReplacer(vector<int>& chalk, int k) {
-        long long total = chalk[0];
+        long long total=0;
         int n = chalk.size();
-        if(n==1) return 0;
-        vector<long long>pref(n);
-        pref[0] = chalk[0];
-        for(int i=1; i<n; i++){
+        for(int i=0; i<n; i++){
             total+=chalk[i];
-            pref[i] = pref[i-1] + chalk[i];
         }
-        if(total>k){
-            for(int i=0; i<n; i++){
-                if(pref[i]>k) return i;
+        long long left = k%total;
+        for(int i=0; i<n; i++){
+            if(left < chalk[i]){
+                return i;
             }
+            left-=chalk[i];
         }
-        else{
-            long long left = k%total;
-            if(left == 0) return 0;
-            int st = 0 , end = n-1;
-            while(st<=end){
-                int mid = st + (end-st)/2;
-                if(left<pref[mid]) end = mid-1;
-                else st = mid+1;
-            }
-            return st;
-        }
-        return -1;
+        return 0;
     }
 };
