54c0bbc4cbf40b3728ea2eba98f8b170dc58c451
Krishna <krishnamall1120@gmail.com>
2026-09-03 19:48:00 +0530

    Time: 4 ms (65.18%) | Memory: 165.9 MB (62.83%) - LeetSync

--- 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.cpp
+++ 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.cpp
@@ -1,40 +1,21 @@
 class Solution {
 public:
-    bool uniformArray(vector<int>& nums) {
-       int min_even = INT_MAX , min_odd = INT_MAX;
-       for(int i=0; i<nums.size(); i++){
-            if(nums[i]%2==0){
-                min_even = min(min_even , nums[i]);
-            } 
-            else{
-                min_odd = min(min_odd , nums[i]);
+    bool uniformArray(vector<int>& nums1) {
+        int mini = INT_MAX;
+        bool odd = false;
+        for(int val : nums1){
+            if(val%2){
+                odd = true;
+                mini = min(mini , val);
             }
-       }
+        }
 
-       bool isAns = true;
-       if(min_even == INT_MAX || min_odd == INT_MAX) return true;
-       else{
-            for(int i=0; i<nums.size(); i++){
-                if(nums[i]%2==0){
-                    if(nums[i]-min_odd<1){
-                        isAns = false;
-                        break;
-                    }
-                }
+        if(!odd) return true;
+        else{
+            for(int val : nums1){
+                if(val%2==0 && mini>val) return false;
             }
-
-            if(isAns) return true;
-            else{
-                for(int i=0; i<nums.size(); i++){
-                if(nums[i]%2==1){
-                    if(nums[i]-min_odd<1){
-                        isAns = false;
-                        break;
-                    }
-                }
-            }
-            }
-       }
-       return isAns;
+        }
+        return true;
     }
 };
