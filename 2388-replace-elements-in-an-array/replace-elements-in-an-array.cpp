f08b4aae9d9db76a0e7b348aa96ea689f9aa8b62
Krishna <krishnamall1120@gmail.com>
2026-09-08 12:30:50 +0530

    Time: 0 ms (100.00%) | Memory: 115.5 MB (99.44%) - LeetSync

--- 2388-replace-elements-in-an-array/replace-elements-in-an-array.cpp
+++ 2388-replace-elements-in-an-array/replace-elements-in-an-array.cpp
@@ -1,14 +1,13 @@
+int m[1000001];
 class Solution {
 public:
     vector<int> arrayChange(vector<int>& nums, vector<vector<int>>& operations) {
-        unordered_map<int,int>m;
         for(int i=0; i<nums.size(); i++){
             m[nums[i]] = i;
         }
         for(int i=0; i<operations.size(); i++){
-            int idx = m[operations[i][0]];
-            nums[idx] = operations[i][1];
-            m[operations[i][1]] = idx;
+            nums[m[operations[i][0]]] = operations[i][1];
+            m[operations[i][1]] = m[operations[i][0]];
         }
         return nums;
     }
